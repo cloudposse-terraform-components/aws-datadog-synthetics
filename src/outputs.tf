cb252406cde05ed5aae536a6f499e3451df1d44b
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:17:38 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,19 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "datadog_synthetics_test_names" {
+  value       = module.datadog_synthetics.datadog_synthetics_test_names
+  description = "Names of the created Datadog synthetic tests"
+}
+
+output "datadog_synthetics_test_ids" {
+  value       = module.datadog_synthetics.datadog_synthetics_test_ids
+  description = "IDs of the created Datadog synthetic tests"
+}
+
+output "datadog_synthetics_test_monitor_ids" {
+  value       = module.datadog_synthetics.datadog_synthetics_test_monitor_ids
+  description = "IDs of the monitors associated with the Datadog synthetics tests"
+}
+
+output "datadog_synthetics_test_maps" {
+  value       = { for v in module.datadog_synthetics.datadog_synthetic_tests : v.name => v.id }
+  description = "Map (name: id) of the created Datadog synthetic tests"
 }
