8699b26f761c360c9289679f7ada257aeae59278
RoseSecurity <michael@rosesecurity.dev>
2025-07-15 11:17:39 -0400

    feat: add core updated logic

--- src/versions.tf
+++ src/versions.tf
@@ -4,11 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.9.0, < 6.0.0"
+      version = ">= 4.9.0"
     }
     datadog = {
       source  = "datadog/datadog"
       version = ">= 3.3.0"
+    }
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 1.12.0"
     }
   }
 }
